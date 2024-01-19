91e081d2e0d1819f54de29da1eb3a392be217ac3
MinjiLEE <alswl9703@naver.com>
2024-01-19 16:55:12 +0900

    S10P11B303-

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+
 </script>
 
 <template>
@@ -12,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
