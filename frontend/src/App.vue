510626e445c3c1dabfd0e2c15353916335f3565b
JaeMin_Kim <jamsince1994@naver.com>
2024-01-22 09:38:17 +0900

    Revert "S10P12B303-92-chore-vuetify설정"
    This reverts commit dd8a20b60bd2373434741c83fb168dca7a8e21d3.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,16 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <v-card
-        loading
-        title="Card title"
-        subtitle="Subtitle"
-        text="..."
-      >
-        <v-card-actions>
-          <v-btn>Click me</v-btn>
-        </v-card-actions>
-      </v-card>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
