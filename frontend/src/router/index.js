41bcca4e5b9384bde5f406676122fbe69ab87a19
JaeMin_Kim <jamsince1994@naver.com>
2024-01-22 16:37:55 +0900

    S10P12B303-180-chore-set-router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,28 +7,304 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'main',
+      component: HomeView,
+      redirect: { name: 'main-default' },
+      children: [
+        {
+          path: 'default',
+          name: 'main-default',
+          component: '',
+        },
+        {
+          path: 'user',
+          name: 'main-user',
+          component: '',
+        },
+        {
+          path: 'vet',
+          name: 'main-vet',
+          component: '',
+        }
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/register',
+      name: 'register',
+      component: "",
+      redirect: { name: 'register-home' },
+      children: [
+        {
+          path: '/',
+          name: 'register-home',
+          component: '',
+        },
+        {
+          path: 'vet',
+          name: 'register-vet',
+          component: '',
+        },
+        {
+          path: 'user',
+          name: 'register-user',
+          component: '',
+          children: [
+            {
+              path: 'pet',
+              name: 'register-pet',
+              component: '',
+            }
+          ],
+        },
+        {
+          path: 'complete',
+          name: 'register-complete',
+          component: '',
+        }
+      ],
     },
-    
-    
-    
-    
-    
-    
-    
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: "",
+      redirect: { name: 'login-home' },
+      children: [
+        {
+          path: '/',
+          name: 'login-home',
+          component: '',
+        },
+        {
+          path: 'pending',
+          name: 'login-pending',
+          component: '',
+        },
+      ],
+    },
+    {
+      path: '/find',
+      name: 'find',
+      component: "",
+      redirect: { name: 'find-id' },
+      children: [
+        {
+          path: 'id',
+          name: 'find-id',
+          component: '',
+        },
+        {
+          path: 'password',
+          name: 'find-password',
+          component: '',
+        },
+      ],
+    },
+    {
+      path: '/reserve',
+      name: 'reserve',
+      component: "",
+      redirect: { name: 'reserve-user' },
+      children: [
+        {
+          path: 'user',
+          name: 'reserve-user',
+          component: '',
+          redirect: { name: '' },
+          children: [
+            {
+              path: 'date',
+              name: 'reserve-user-date',
+              component: '',
+            },
+            {
+              path: 'hospital',
+              name: 'reserve-user-hospital',
+              component: '',
+            },
+            {
+              path: 'time',
+              name: 'reserve-user-time',
+              component: '',
+            },
+            {
+              path: 'pet',
+              name: 'reserve-user-pet',
+              component: '',
+            },
+            {
+              path: 'complete',
+              name: 'reserve-user-complete',
+              component: '',
+            },
+          ],
+        },
+        {
+          path: 'vet',
+          name: 'reserve-vet',
+          component: '',
+        },
+        {
+          path: 'emergency',
+          name: 'reserve-emergency',
+          component: '',
+        },
+      ],
+    },
+    {
+      path: '/cam',
+      name: 'cam',
+      component: '',
+      redirect: { name: 'cam-consult' },
+      children: [
+        {
+          path: 'consult',
+          name: 'cam-consult',
+          component: '',
+        },
+        {
+          path: 'broadcast',
+          name: 'cam-broadcast',
+          component: '',
+        }
+      ],
+    },
+    {
+      path: '/lifecare',
+      name: 'lifecare',
+      component: '',
+      redirect: { name: 'lifecare-community' },
+      children: [
+        {
+          path: 'community',
+          name: 'lifecare-community',
+          component: '',
+          redirect: { name: '' },
+          children: [
+            {
+              path: 'list',
+              name: 'lifecare-community-list',
+              component: '',
+            },
+            {
+              path: 'detail/:listno',
+              name: 'lifecare-venue-detail',
+              component: '',
+            },
+          ],
+        },
+        {
+          path: 'venue',
+          name: 'lifecare-venue',
+          component: '',
+        },
+        {
+          path: 'broadcast',
+          name: 'lifecare-venue',
+          component: '',
+          children: [
+            {
+              path: 'enter',
+              name: 'lifecare-broadcast-enter',
+              component: '',
+            },
+          ],
+        }
+      ],
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: "",
+      redirect: { name: 'mypage-user' },
+      children: [
+        {
+          path: 'user',
+          name: 'mypage-user',
+          component: '',
+          redirect: { name: 'mypage-user-info' },
+          children: [
+            {
+              path: 'info',
+              name: 'mypage-user-info',
+              component: '',
+            },
+            {
+              path: 'list',
+              name: 'mypage-user-list',
+              component: '',
+              redirect: { name: 'mypage-user-list-current' },
+              children: [
+                {
+                  path: 'current',
+                  name: 'mypage-user-list-current',
+                  component: '',
+                  
+                },
+                {
+                  path: 'past',
+                  name: 'mypage-user-list-past',
+                  component: '',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: 'vet',
+          name: 'mypage-user',
+          component: '',
+          redirect: { name: 'mypage-vet-info' },
+          children: [
+            {
+              path: 'info',
+              name: 'mypage-vet-info',
+              component: '',
+            },
+            {
+              path: 'list',
+              name: 'mypage-vet-list',
+              component: '',
+              redirect: { name: 'mypage-vet-list-current' },
+              children: [
+                {
+                  path: 'current',
+                  name: 'mypage-vet-list-current',
+                  component: '',
+                  
+                },
+                {
+                  path: 'past',
+                  name: 'mypage-vet-list-past',
+                  component: '',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: "",
+      redirect: { name: 'admin-account' },
+      children: [
+        {
+          path: 'account',
+          name: 'admin-account',
+          component: "",
+        },
+        {
+          path: 'reserve',
+          name: 'admin-reserve',
+          component: "",
+        },
+        {
+          path: 'community',
+          name: 'admin-community',
+          component: "",
+        }
+      ]
     }
   ]
 })
