77f58e7bbc71245d607009bd33b159f2735814cc
JaeMin_Kim <jamsince1994@naver.com>
2024-01-22 09:38:01 +0900

    Revert "S10P11B303-"
    This reverts commit 91e081d2e0d1819f54de29da1eb3a392be217ac3.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-
-
 </script>
 
 <template>
@@ -24,7 +22,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
