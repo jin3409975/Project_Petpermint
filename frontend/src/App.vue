b9f05cff0df6ead145a46c913803c55253bef184
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-22 16:32:08 +0900

    S10P12B303-refactor:v-img 코드 개선

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <template>
     <v-app>
         <v-app-bar app fixed>
-        <!-- 왼쪽 항목: 홈, 상담 예약 등 -->
-        <v-toolbar-items>
+        <div class="flex-container">
+            <!-- 왼쪽 항목 -->
+            <v-toolbar-items>
             <v-btn text>Home</v-btn>
             <v-btn text>초기 상담 예약</v-btn>
             <v-btn text>병원 진료 예약</v-btn>
             <v-btn text>라이프케어</v-btn>
-        </v-toolbar-items>
-    
+            </v-toolbar-items>
+        </div>
         <!-- 로고 이미지 -->
         <v-img
             :src="logoUrl"
@@ -16,11 +17,13 @@
             contain
             height="40"
         ></v-img>
-
-    
-        <!-- 오른쪽 항목: 로그인, 회원가입 버튼 -->
-        <v-btn text>로그인</v-btn>
-        <v-btn text>회원가입</v-btn>
+        <div class="flex-container">
+            <!-- 오른쪽 항목 -->
+            <v-toolbar-items>
+            <v-btn text>로그인</v-btn>
+            <v-btn text>회원가입</v-btn>
+            </v-toolbar-items>
+        </div>
         </v-app-bar>
     </v-app>
 </template>
@@ -30,10 +33,13 @@
 export default {
 computed: {
     logoUrl() {
-    return new URL('@/assets/img/logo.png', import.meta.url).href;
+    return new URL('@/assets/img/logo.png', import.meta.url).href
     }
 }
 }
 </script>
 <style>
+.flex-container {
+    flex: 1
+}
 </style>
