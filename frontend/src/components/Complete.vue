b365b9ebd3f8d1d61ca93b26989fa33bc63f08c2
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-23 14:31:55 +0900

    S10P12B303-style:코드컨벤션

--- frontend/src/components/Complete.vue
+++ frontend/src/components/Complete.vue
@@ -1,13 +1,9 @@
 <template>
     <div>
-        <p> 회원 가입 완료 되었습니다.</p>
+        <p>회원 가입 완료 되었습니다.</p>
     </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
