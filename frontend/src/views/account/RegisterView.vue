396e28a8c585b75a44ed6672db4d0fdc6a8c0865
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-24 14:28:16 +0900

    S10P12B303-161-style-router사용

--- frontend/src/views/account/RegisterView.vue
+++ frontend/src/views/account/RegisterView.vue
@@ -1,25 +1,7 @@
-<script setup>
-import { ref } from 'vue'
-import RegisterType from '@/components/register/RegisterType.vue'
-import RegisterVet from '@/components/register/RegisterVet.vue'
-import RegisterUser from '@/components/register/RegisterUser.vue'
-
-const selectedType = ref('')
-
-const setType = (type) => {
-    selectedType.value = type
-}
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        <!-- vet과 user중에 하나를 선택 -->
-        <RegisterType v-if="selectedType === ''" @select-type="setType" />
-        <!-- vet이면 보여지는 컴포넌트 -->
-        <RegisterVet v-if="selectedType === 'vet'" />
-        <!-- user이면 보여지는 컴포넌트 -->
-        <RegisterUser v-if="selectedType === 'user'" />
-    </div>
+    <router-view></router-view>
 </template>
 
 <style></style>
