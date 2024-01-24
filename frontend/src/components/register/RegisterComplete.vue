ed0767ef0def6d365b9a3d184570afbb3d251e7d
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-24 16:13:13 +0900

    S10P12B303-161-feat-홈버튼생성

--- frontend/src/components/register/RegisterComplete.vue
+++ frontend/src/components/register/RegisterComplete.vue
@@ -1,9 +1,31 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const navigateToMain = () => {
+  router.push({ name: 'main-home' })
+}
+</script>
 
 <template>
-    <div>
-        <p>회원 가입 완료 되었습니다.</p>
+  <div class="centered-container">
+    <div class="container-margin">
+      <h1>회원 가입 완료 되었습니다.</h1>
     </div>
+    <v-btn text @click="navigateToMain">홈으로 가기</v-btn>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+.centered-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+}
+.container-margin {
+  margin-bottom: 50px;
+}
+</style>
