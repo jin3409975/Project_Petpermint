1ec23ee9a9a2d406d3320980a595e7fe1f9c0c18
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-24 15:02:33 +0900

    S10P12B303-161-style-버튼재배열

--- frontend/src/components/register/RegisterType.vue
+++ frontend/src/components/register/RegisterType.vue
@@ -1,13 +1,23 @@
-<script setup></script>
+<script setup>
+const emit = defineEmits(['select-type'])
+
+const selectType = (type) => {
+    emit('select-type', type)
+}
+</script>
 
 <template>
-    <div>
-        <!-- 회원가입 뷰
-        <RouterLink to="/register/vet">수의사</RouterLink> -->
-
-        <RouterLink to="/register/user">일반회원</RouterLink>
-        <p>수의사 회원가입</p>
+    <div class="d-flex">
+        <v-btn prepend-icon="$vuetify" variant="outlined" @click="selectType('vet')">수의사</v-btn>
+        <v-btn prepend-icon="$vuetify" variant="outlined" @click="selectType('user')">일반 유저</v-btn>
     </div>
 </template>
 
-<style scoped></style>
+<style>
+.d-flex {
+    display: flex;
+    justify-content: center; /* 수직 정렬 */
+    align-items: center; /* 수평 정렬 */
+    height: 100vh; /* 전체 뷰포트 높이 */
+}
+</style>
