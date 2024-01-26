d1266727707ce434612f72766047660e60805ceb
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-26 15:06:23 +0900

    S10P12B303-164-feat-로그인 스켈레톤 작성

--- frontend/src/components/login/Login.vue
+++ frontend/src/components/login/Login.vue
@@ -1,7 +1,38 @@
-<script setup></script>
+<script setup>
+import { useAccountStore } from '@/stores/account.js'
+import { ref } from 'vue'
+
+const store = useAccountStore()
+const useremail = ref(null)
+const password = ref(null)
+
+const logIn = function () {
+  const logindata = {
+    useremail: useremail.value,
+    password: password.value
+  }
+  store.logIn(logindata)
+}
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <div>
+      <!-- 이메일 , 비밀번호 입력 폼  -->
+      <form @submit.prevent="logIn">
+        <div class="logininput">
+          email
+          <input type="text" placeholder="email" v-model.trim="useremail" />
+        </div>
+        <div class="logininput">
+          password
+          <input type="text" placeholder="password" v-model.trim="password" />
+        </div>
+
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style></style>
