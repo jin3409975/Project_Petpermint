01d5473c69041f770c51c2a122516700cdde2b15
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-26 15:07:37 +0900

    S10P12B303-164-feat-router사용

--- frontend/src/views/account/LoginView.vue
+++ frontend/src/views/account/LoginView.vue
@@ -1,38 +1,7 @@
-<script setup>
-import { useAccountStore } from '@/stores/account.js'
-import { ref } from 'vue'
-
-const store = useAccountStore()
-const useremail = ref(null)
-const password = ref(null)
-
-const logIn = function () {
-  const logindata = {
-    useremail: useremail.value,
-    password: password.value
-  }
-  store.logIn(logindata)
-}
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <div>
-      <!-- 이메일 , 비밀번호 입력 폼  -->
-      <form @submit.prevent="logIn">
-        <div class="logininput">
-          email
-          <input type="text" placeholder="email" v-model.trim="useremail" />
-        </div>
-        <div class="logininput">
-          password
-          <input type="text" placeholder="password" v-model.trim="password" />
-        </div>
-
-        <button type="submit">로그인</button>
-      </form>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
-<style></style>
+<style scoped></style>
