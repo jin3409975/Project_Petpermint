f9293bf2aa964d98fe99871bd31ff5fd547c515b
MinjiLEE <alswl9703@naver.com>
2024-01-29 13:26:19 +0900

    S10P11B303-

--- frontend/src/components/mypage/MypageUserListCurrent.vue
+++ frontend/src/components/mypage/MypageUserListCurrent.vue
@@ -1,95 +1,31 @@
 <script>
-import { useDate } from 'vuetify'
-import { createVuetify } from 'vuetify'
-
-export const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'light'
-    //
-  }
-})
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { myPageStore } from '@/stores/mypage.js'
+import { watchEffect, ref } from 'vue'
 
 export default {
-  data: () => ({
-    type: 'week',
-    types: ['month', 'week', 'day'],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { title: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { title: 'Mon, Wed, Fri', value: [1, 3, 5] }
-    ],
-    value: [new Date()],
-    events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    titles: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party']
-  }),
-  mounted() {
-    const adapter = useDate()
-    this.getEvents({
-      start: adapter.startOfDay(adapter.startOfMonth(new Date())),
-      end: adapter.endOfDay(adapter.endOfMonth(new Date()))
-    })
+  components: {
+    FullCalendar
   },
-  methods: {
-    getEvents({ start, end }) {
-      const events = []
+  setup() {
+    const mypagestore = myPageStore()
 
-      const min = start
-      const max = end
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
+    // Use a ref for calendarOptions to ensure reactivity
+    const calendarOptions = ref({
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridWeek', // 월별 보기 initialView: 'dayGridMonth'
+      weekends: true,
+      selectable: true,
+      events: mypagestore.myevents
+    })
 
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
-
-        events.push({
-          title: this.titles[this.rnd(0, this.titles.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          allDay: !allDay
-        })
-      }
-
-      this.events = events
-    },
-    getEventColor(event) {
-      return event.color
-    },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
-    }
+    return { calendarOptions }
   }
 }
 </script>
+
 <template>
-  <div>
-    <v-sheet tile height="54" class="d-flex">
-      <v-select
-        v-model="weekday"
-        :items="weekdays"
-        dense
-        variant="outlined"
-        hide-details
-        label="weekdays"
-        class="ma-2"
-      ></v-select>
-    </v-sheet>
-    <v-sheet>
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :weekdays="weekday"
-        :view-mode="type"
-        :events="events"
-      ></v-calendar>
-    </v-sheet>
-  </div>
+  <h1>나의 예약 현황</h1>
+  <FullCalendar :options="calendarOptions"> </FullCalendar>
 </template>
-<style></style>
