5b126cf1af4c0fd459f98aadac46a2612db25906
CHAEEUN LEE <cheeun0903@gmail.com>
2024-01-31 17:38:00 +0900

    S10P12B303-291-feat-tab function completed

--- frontend/src/components/register/RegisterUser.vue
+++ frontend/src/components/register/RegisterUser.vue
@@ -1,45 +1,57 @@
-
 <script setup>
-import { reactive, ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAccountStore } from '@/stores/account.js'
-// import Complete from '@/components/register/Complete.vue'
+
+// Props 정의
+const props = defineProps({
+  title: String
+})
 
 const router = useRouter()
 const store = useAccountStore()
 
-
-const email = ref(null)
-const confirm = ref(null)
+const email = ref('')
+const confirm = ref('')
 const password1 = ref(null)
 const password2 = ref(null)
 const name = ref(null)
 const phone = ref(null)
 const address1 = ref(null)
 const address2 = ref(null)
-const picture = ref(null);
+const picture = ref(null)
 
-const pets = ref([createPet()])
+const pets = ref([{ id: 1, radios: '', petname: '', petage: '', more: '', name: 'Pet 1' }])
 const visible = ref(false)
 
-// 펫 정보
-function createPet() {
-  return reactive({
-    radios: '',
-    petname: '',
-    petage: '',
-    more: ''
-  })
-}
+// 탭
+const tab = ref(null)
+
+// 탭의 개수를 결정하는 계산된 속성
+const length = computed(() => pets.value.length)
+
+// 각 펫의 petname을 감시하고, 변경 시 해당 탭 제목을 업데이트
+watch(
+  pets,
+  (newPets) => {
+    newPets.forEach((pet, index) => {
+      if (pet.petname) {
+        pet.name = pet.petname // 사용자가 입력한 이름으로 탭 제목 업데이트
+      } else {
+        pet.name = `Pet ${index + 1}` // petname이 비어있을 경우 기본 이름 "Pet X" 형식으로 설정
+      }
+    })
+  },
+  { deep: true }
+)
 
 // 펫 정보 폼 추가
-function addPetForm() {
-  // 현재 폼의 개수가 5개 미만이고 모든 필수 입력값이 채워져 있을 때만 새로운 폼 추가
-  if (
-    pets.value.length < 3 &&
-    pets.value.every((pet) => pet.radios && pet.petname && pet.petage && pet.more)
-  ) {
-    pets.value.push(createPet())
+const addPetForm = () => {
+  if (pets.value.length < 5) {
+    const newId = pets.value.reduce((maxId, pet) => Math.max(maxId, pet.id), 0) + 1
+    const newName = `Pet ${newId}`
+    pets.value.push({ id: newId, name: newName, petname: '', petage: '', more: '' })
+    tab.value = newId - 1 // 새롭게 추가된 탭으로 이동 (0부터 시작하는 인덱스로 변환)
   } else if (pets.value.length >= 3) {
     // 폼의 개수가 최대치에 도달했을 경우
     alert('폼을 더 이상 추가할 수 없습니다. 최대 3개까지만 추가 가능합니다.')
@@ -91,26 +103,31 @@ const rules = {
 //   }
 // }
 
-const emailRequest = function () {
-  console.log('이메일 인증 요청')
-  store.emailRequest(email.value)
+const emailStatus = ref('request')
+
+const emailAction = () => {
+  if (emailStatus.value === 'request') {
+    console.log('이메일 인증 요청')
+    store.emailRequest(email.value)
+    emailStatus.value = 'validate'
+  } else if (emailStatus.value === 'validate') {
+    console.log('이메일 확인 요청')
+    store.emailValidate(email.value, confirm.value)
+    emailStatus.value = 'completed'
+  }
 }
 
-const emailValidate = function () {
-  console.log('이메일 확인 요청')
-  store.emailValidate(email.value, confirm.value)
-}
-
-const getFile = function (event) {
-  console.log(event)
-  picture.value = event.target.files[0]
-}
+// 채은: 프로필수정은 마이페이지에서 하는게 좋을듯..
+// const getFile = function (event) {
+//   console.log(event)
+//   picture.value = event.target.files[0]
+// }
 
 const showSnackbar = ref(false)
 
 function register() {
   // 회원가입 처리 로직 여기에 추가
-    if(!store.result) {
+  if (!store.result) {
     alert('이메일 인증을 진행 해주세요')
     return false
   }
@@ -123,7 +140,7 @@ function register() {
     phone: phone.value,
     address1: address1.value,
     address2: address2.value,
-    picture : picture.value
+    picture: picture.value
   }
   console.log(payload)
   store.usersignup(payload)
@@ -134,7 +151,6 @@ function register() {
   setTimeout(() => {
     router.push({ name: 'main-home' })
   }, 2000)
-
 }
 </script>
 
@@ -191,9 +207,33 @@ function register() {
                     variant="outlined"
                     color="#668ba7"
                     bg-color="transparent"
-                  />
-                  <v-btn @click="emailRequest()">인증 요청</v-btn>
-                  <v-btn @click="emailValidate()">인증 확인</v-btn>
+                    :readonly="emailStatus === 'validate' || emailStatus === 'completed'"
+                  >
+                    <template v-slot:append>
+                      <v-btn
+                        v-if="emailStatus === 'request'"
+                        small
+                        @click="emailAction"
+                        style="box-shadow: none; color: #668ba7"
+                      >
+                        인증 요청
+                      </v-btn>
+                      <v-btn
+                        v-else-if="emailStatus === 'validate'"
+                        small
+                        @click="emailAction"
+                        style="box-shadow: none; color: #668ba7"
+                      >
+                        인증 확인
+                      </v-btn>
+                      <v-icon
+                        v-else-if="emailStatus === 'completed'"
+                        size="large"
+                        color="#43A047"
+                        icon="mdi-check-circle"
+                      ></v-icon>
+                    </template>
+                  </v-text-field>
                 </v-col>
 
                 <v-col cols="12" md="6">
@@ -225,11 +265,10 @@ function register() {
                 </v-col>
               </v-row>
             </v-card>
-            <div class="button-row">
+            <div class="d-flex justify-space-between button-row">
               <v-btn @click="goToType" style="box-shadow: none" variant="outlined" color="#757575"
                 >돌아가기</v-btn
               >
-              <v-spacer></v-spacer>
               <v-btn @click="nextStep" style="box-shadow: none" variant="flat" color="#668ba7"
                 >다음</v-btn
               >
@@ -284,10 +323,11 @@ function register() {
                     bg-color="transparent"
                   />
                 </v-col>
-                <input type="file" v-on:change="getFile">프로필 사진 업로드</input>
+                <!-- 채은: 마이페이지에서 프로필 수정하는게 좋을듯.. -->
+                <!-- <input type="file" v-on:change="getFile">프로필 사진 업로드</input> -->
               </v-row>
             </v-card>
-            <div class="button-row">
+            <div class="d-flex justify-space-between button-row">
               <v-btn
                 @click="previousStep"
                 style="box-shadow: none"
@@ -306,77 +346,78 @@ function register() {
             <v-card flat>
               <h4 class="text-h4 mb-3">마이 펫 정보</h4>
               <p class="mb-5">내 반려동물의 정보를 입력해 주세요.</p>
-              <div v-for="(pet, index) in pets" :key="index" class="pet-form">
-                <v-row class="mt-2">
-                  <v-col cols="12" md="12" style="padding-top: 0%; padding-bottom: 0%">
-                    <v-btn
-                      class="custom-icon-color"
-                      icon="mdi-minus-box-multiple"
-                      size="large"
-                      @click="removePetForm(index)"
-                      style="box-shadow: none; position: absolute; top: 0; right: 0"
-                    ></v-btn>
-                    <v-col style="padding-bottom: 0%">
-                      <p>반려동물 종류</p>
-                    </v-col>
-                    <v-radio-group inline v-model="pet.radios">
-                      <v-col cols="12" md="4" style="padding-bottom: 0%">
-                        <v-radio label="강아지" value="강아지"></v-radio>
-                      </v-col>
-                      <v-col cols="12" md="4">
-                        <v-radio label="고양이" value="고양이"></v-radio>
-                      </v-col>
-                      <v-col cols="12" md="4">
-                        <v-radio label="기타" value="기타"></v-radio>
-                      </v-col>
-                    </v-radio-group>
-                  </v-col>
+              <!-- Tabs -->
+              <v-tabs v-model="tab">
+                <v-tab v-for="pet in pets" :key="pet.id" :value="pet.id">{{ pet.name }}</v-tab>
+                <v-tab v-if="pets.length < 5" @click="addPetForm">+</v-tab>
+              </v-tabs>
+              <!-- Tabs 내용 -->
+              <v-card-text>
+                <v-window v-model="tab">
+                  <v-window-item v-for="pet in pets" :key="pet.id" :value="pet.id">
+                    <!-- 반려동물 정보 입력 폼 -->
+                    <div class="pet-form">
+                      <v-row class="mt-2">
+                        <v-col cols="12" md="12" style="padding-top: 0%; padding-bottom: 0%">
+                          <v-btn
+                            class="custom-icon-color"
+                            icon="mdi-minus-box-multiple"
+                            size="large"
+                            @click="removePetForm(index)"
+                            style="box-shadow: none; position: absolute; top: 0; right: 0"
+                          ></v-btn>
+                          <v-col style="padding-bottom: 0%">
+                            <p>반려동물 종류</p>
+                          </v-col>
+                          <v-radio-group inline v-model="pet.radios">
+                            <v-col cols="12" md="4" style="padding-bottom: 0%">
+                              <v-radio label="강아지" value="강아지"></v-radio>
+                            </v-col>
+                            <v-col cols="12" md="4">
+                              <v-radio label="고양이" value="고양이"></v-radio>
+                            </v-col>
+                            <v-col cols="12" md="4">
+                              <v-radio label="기타" value="기타"></v-radio>
+                            </v-col>
+                          </v-radio-group>
+                        </v-col>
 
-                  <v-col cols="12" md="6">
-                    <v-text-field
-                      label="반려동물 이름 "
-                      :rules="[rules.required]"
-                      v-model="pet.petname"
-                      variant="outlined"
-                      color="#668ba7"
-                      bg-color="transparent"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6">
-                    <v-text-field
-                      label="반려동물 나이"
-                      v-model="pet.petage"
-                      variant="outlined"
-                      color="#668ba7"
-                      bg-color="transparent"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="12">
-                    <v-text-field
-                      label="특이사항"
-                      v-model="pet.more"
-                      placeholder="특이사항이 없을 시 '없음'으로 작성해주세요."
-                      variant="outlined"
-                      color="#668ba7"
-                      bg-color="transparent"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <div class="text-center">
-                  <v-btn
-                    @click="addPetForm"
-                    color="medium-emphasis"
-                    rounded
-                    variant="outlined"
-                    class="ma-2 mt-5"
-                  >
-                    펫 추가하기
-                    <v-icon end icon="mdi-plus-box-multiple-outline"></v-icon>
-                  </v-btn>
-                </div>
-              </div>
+                        <v-col cols="12" md="6">
+                          <v-text-field
+                            label="반려동물 이름 "
+                            :rules="[rules.required]"
+                            v-model="pet.petname"
+                            variant="outlined"
+                            color="#668ba7"
+                            bg-color="transparent"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="6">
+                          <v-text-field
+                            label="반려동물 나이"
+                            v-model="pet.petage"
+                            variant="outlined"
+                            color="#668ba7"
+                            bg-color="transparent"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="12">
+                          <v-text-field
+                            label="특이사항"
+                            v-model="pet.more"
+                            placeholder="특이사항이 없을 시 '없음'으로 작성해주세요."
+                            variant="outlined"
+                            color="#668ba7"
+                            bg-color="transparent"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </div>
+                  </v-window-item>
+                </v-window>
+              </v-card-text>
             </v-card>
-            <div class="button-row">
+            <div class="d-flex justify-space-between last-btn">
               <v-btn
                 @click="previousStep"
                 style="box-shadow: none"
@@ -420,7 +461,12 @@ function register() {
 .v-input__details {
   display: none !important;
 }
+.last-btn {
+  height: 0% !important;
+  margin-top: 0px;
+}
 .button-row {
+  height: 0% !important;
   margin-top: 30px;
 }
 .pet-form {
