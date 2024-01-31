9b234957bf0662e08a4e48dedadaf80e52bc8757
JIEUN LEE <jin3409975@gmail.com>
2024-01-31 17:41:44 +0900

    S10P12B303-288-design-init

--- frontend/src/components/register/RegisterType.vue
+++ frontend/src/components/register/RegisterType.vue
@@ -12,27 +12,43 @@ const navigateToRegisterVet = () => {
 </script>
 
 <template>
-  <div>
+  <div class="page-container">
     <h1 class="header">회원가입</h1>
     <div class="card-container">
       <v-container>
-        <v-card @click="navigateToRegisterUser" class="register-card">보호자</v-card>
+        <v-card @click="navigateToRegisterUser" class="register-card user-card" elevation="5">
+          <img src="@/assets/img/animal-icon-1.png" alt="user-icon" />
+          <v-card-title>보호자</v-card-title>
+          <v-card-text class="register-card-text">
+            펫퍼민트만의 특별한 혜택을 경험해보세요!
+          </v-card-text>
+        </v-card>
       </v-container>
 
       <v-container>
-        <v-card @click="navigateToRegisterVet" class="register-card">수의사</v-card>
+        <v-card @click="navigateToRegisterVet" class="register-card vet-card" elevation="5">
+          <img src="@/assets/img/hospital-icon-1.png" alt="hospital-icon" />
+          <v-card-title>수의사</v-card-title>
+          <v-card-text class="register-card-text"> Oh! Hello Vue.js! </v-card-text>
+        </v-card>
       </v-container>
     </div>
   </div>
 </template>
 
 <style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 .header {
   text-align: center;
   font-weight: bold;
   font-size: 30px; /* 원하는 크기로 조절 */
   margin-top: 50px;
-  margin-bottom: 50px;
+  margin-bottom: 15px;
 }
 
 .card-container {
@@ -42,7 +58,26 @@ const navigateToRegisterVet = () => {
 }
 
 .register-card {
-  height: 300px;
-  width: 400px;
+  height: 400px;
+  width: 300px;
+  text-align: center;
+}
+
+.user-card {
+  margin-right: 50%; /* 보호자 카드 오른쪽 부분과 수의사 카드 왼쪽 부분 간격 조절 */
+}
+
+.vet-card {
+  margin-left: 50px; /* 보호자 카드 오른쪽 부분과 수의사 카드 왼쪽 부분 간격 조절 */
+}
+
+.register-card-text {
+  margin-top: 50px;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+  margin-top: 20px;
 }
 </style>
