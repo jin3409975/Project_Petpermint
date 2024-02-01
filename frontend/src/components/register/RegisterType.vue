60f83713e305eb673af76de44f4cf29e809aee9b
JIEUN LEE <jin3409975@gmail.com>
2024-02-01 15:50:29 +0900

    S10P12B303-288-test

--- frontend/src/components/register/RegisterType.vue
+++ frontend/src/components/register/RegisterType.vue
@@ -27,7 +27,7 @@ const navigateToRegisterVet = () => {
               1. 원하는 수의사와 함께하는 화상 상담
               <br />
               <br />
-              2. 간편해진 방문 병원 진료 예약
+              2. 간편한 병원 진료 예약
               <br />
               <br />
               3. 펫퍼민트만의 라이프 케어
@@ -41,7 +41,7 @@ const navigateToRegisterVet = () => {
           <img src="@/assets/img/hospital-icon-1.png" alt="hospital-icon" />
           <v-card-title>수의사</v-card-title>
           <v-card class="inner-card" color="#BBDEFB">
-            <v-card-text class="register-card-text"> Oh! Hello Vue.js! </v-card-text>
+            <v-card-text class="register-card-text"> 언 </v-card-text>
           </v-card>
         </v-card>
       </v-container>
