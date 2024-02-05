7ffd87a107f76e20e2229394eebc846210adee19
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-05 14:30:01 +0900

    S10P12B303-383-feat-delete navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,6 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const hideNavbar = computed(() => route.meta.hideNavbar)
 const hideFooter = computed(() => route.meta.hideFooter)
 </script>
 
