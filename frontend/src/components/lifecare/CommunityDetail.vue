76c2ace970b2127111b42c02b41af20bc8622091
govltjsdnd24 <gragasolaf@gmail.com>
2024-02-05 16:03:47 +0900

    S10P11B303-

--- frontend/src/components/lifecare/CommunityDetail.vue
+++ frontend/src/components/lifecare/CommunityDetail.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+const props = defineProps(['article'])
+
+const checkLike = async function (postId, userId) {
+  try {
+    article.value = await stores.communitydetail(postId)
+  } catch (error) {
+    console.error('Error fetching community detail:', error)
+  }
+}
+</script>
 
 <template>
-  <div></div>
+  <v-container fluid class="w-50">
+    <div class="mx-auto">{{ article.content }}</div>
+    Likes: {{ article.likes }} Views: {{ article.hits }}
+    <v-btn>Like</v-btn>
+    <v-btn>Back</v-btn>
+  </v-container>
 </template>
 
 <style scoped></style>
