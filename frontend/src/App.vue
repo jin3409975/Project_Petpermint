66680fb867a1d2241ad54d8c7356b4186f019322
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-05 17:15:29 +0900

    S10P12B303-384-feat-navbarmain created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
+import NavbarMain from '@/components/main/NavbarMain.vue'
 import Footer from '@/components/main/Footer.vue'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+const hideNavbarMain = computed(() => route.meta.hideNavbarMain)
 const hideFooter = computed(() => route.meta.hideFooter)
 </script>
 
 <template>
   <body>
     <v-app>
+      <NavbarMain v-if="!hideNavbarMain" />
       <RouterView />
       <Footer v-if="!hideFooter" />
     </v-app>
