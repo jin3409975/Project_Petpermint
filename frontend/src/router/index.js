b7e62082a20aa2e0b6f600c3e58e3e689864dcad
cyberjam <jamsince1994@naver.com>
2024-02-06 21:21:30 +0900

    S10P12B303-192-revert-index-to-00ecc8f434583039d40997167bb08fe9e3cf6192

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,7 +6,6 @@ import Login from '@/components/login/Login.vue'
 import MainView from '@/views/MainView.vue'
 
 import Main from '@/components/main/Main.vue'
-import MainUser from '@/components/main/MainUser.vue'
 import RegisterVet from '@/components/register/RegisterVet.vue'
 // import RegisterComplete from '@/components/register/RegisterComplete.vue'
 import RegisterUserView from '@/views/RegisterUserView.vue'
@@ -20,7 +19,6 @@ import ReserveAppointView from '@/views/ReserveAppointView.vue'
 import ReserveHospital from '@/components/reserve/ReserveHospital.vue'
 import ReserveTime from '@/components/reserve/ReserveTime.vue'
 import ReservePet from '@/components/reserve/ReservePet.vue'
-// import ReserveComplete from '@/components/reserve/ReserveComplete.vue'
 
 import AdminViewVue from '@/views/AdminView.vue'
 import AdminAcountVue from '@/components/admin/AdminAcount.vue'
@@ -30,18 +28,16 @@ import MypageVetView from '@/views/MypageVetView.vue'
 import MypageUserInfo from '@/components/mypage/MypageUserInfo.vue'
 import MypageUserListView from '@/views/MypageUserListView.vue'
 import MypageUserListCurrent from '@/components/mypage/MypageUserListCurrent.vue'
-// import MypageUserListPast from '@/components/mypage/MypageUserListPast.vue'
 import MypageVetInfo from '@/components/mypage/MypageVetInfo.vue'
 import MypageVetListCurrent from '@/components/mypage/MypageVetListCurrent.vue'
-// import MypageVetListPast from '@/components/mypage/MypageVetListPast.vue'
 
 import FindId from '@/components/login/FindId.vue'
 import FindPassword from '@/components/login/FindPassword.vue'
 import FindinfoView from '@/views/account/FindinfoView.vue'
 
-import LifecareViewVue from '@/views/LifecareView.vue'
-import VenueMapVue from '@/components/lifecare/VenueMap.vue'
+import CamBroadcastVue from '@/components/cam/CamBroadcast.vue'
 
+import MainVet from '@/components/main/MainVet.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -59,19 +55,19 @@ const router = createRouter({
           children: [
             {
               path: '',
+              meta: { hideNavbarMain: true },
               name: 'main-home',
               component: Main
             },
             {
               path: 'user',
               name: 'main-user',
-              meta: { hideNavbarMain: true },
-              component: MainUser
+              component: ''
             },
             {
               path: 'vet',
               name: 'main-vet',
-              component: ''
+              component: MainVet
             }
           ]
         },
@@ -90,32 +86,22 @@ const router = createRouter({
             {
               path: 'vet',
               name: 'register-vet',
-              meta: { hideNavbar: true },
+              meta: { hideNavbarMain: true, hideNavbar: true },
               component: RegisterVet
             },
             {
               path: 'user',
               name: 'register-user',
+              meta: { hideNavbarMain: true },
               component: RegisterUserView,
-              meta: { hideNavbar: true },
               redirect: { name: 'register-info' },
               children: [
                 {
                   path: 'info',
                   name: 'register-info',
                   component: RegisterUser
-                },
-                {
-                  path: 'pet',
-                  name: 'register-pet',
-                  component: ''
                 }
               ]
-            },
-            {
-              path: 'complete',
-              name: 'register-complete',
-              component: ''
             }
           ]
         },
@@ -184,11 +170,11 @@ const router = createRouter({
                 }
               ]
             },
-            // {
-            //   path: 'complete',
-            //   name: 'reserve-initial-complete',
-            //   component: ReserveComplete
-            // },
+            {
+              path: 'complete',
+              name: 'reserve-initial-complete',
+              component: ''
+            },
             {
               path: 'appoint',
               name: 'reserve-appoint',
@@ -209,12 +195,12 @@ const router = createRouter({
                   path: 'pet',
                   name: 'reserve-appoint-pet',
                   component: ReservePet
+                },
+                {
+                  path: 'complete',
+                  name: 'reserve-appoint-complete',
+                  component: ''
                 }
-                // {
-                //   path: 'complete',
-                //   name: 'reserve-appoint-complete',
-                //   component: ReserveComplete
-                // }
               ]
             },
             {
@@ -232,6 +218,7 @@ const router = createRouter({
         {
           path: '/cam',
           name: 'cam',
+          meta: { hideNavbarMain: true },
           component: '',
           redirect: { name: 'cam-consult' },
           children: [
@@ -243,15 +230,15 @@ const router = createRouter({
             {
               path: 'broadcast',
               name: 'cam-broadcast',
-              component: ''
+              component: CamBroadcastVue
             }
           ]
         },
         {
           path: '/lifecare',
           name: 'lifecare',
-          component: LifecareViewVue,
-          redirect: { name: 'lifecare-venue' },
+          component: '',
+          redirect: { name: 'lifecare-community' },
           children: [
             {
               path: 'community',
@@ -274,11 +261,11 @@ const router = createRouter({
             {
               path: 'venue',
               name: 'lifecare-venue',
-              component: VenueMapVue
+              component: ''
             },
             {
               path: 'broadcast',
-              name: 'lifecare-broadcast',
+              name: 'lifecare-venue',
               component: '',
               children: [
                 {
@@ -318,11 +305,6 @@ const router = createRouter({
                       name: 'mypage-user-list-current',
                       component: MypageUserListCurrent
                     }
-                    // {
-                    //   path: 'past',
-                    //   name: 'mypage-user-list-past',
-                    //   component: MypageUserListPast
-                    // }
                   ]
                 }
               ]
@@ -349,11 +331,6 @@ const router = createRouter({
                       name: 'mypage-vet-list-current',
                       component: MypageVetListCurrent
                     }
-                    // {
-                    //   path: 'past',
-                    //   name: 'mypage-vet-list-past',
-                    //   component: MypageVetListPast
-                    // }
                   ]
                 }
               ]
@@ -364,6 +341,7 @@ const router = createRouter({
           path: '/admin',
           name: 'admin',
           component: AdminViewVue,
+          meta: { hideNavbarMain: true },
           redirect: { name: 'admin-account' },
           children: [
             {
