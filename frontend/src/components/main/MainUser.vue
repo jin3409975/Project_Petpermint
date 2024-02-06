5bde92f2b7d1e13678bfe5ab47a0e6ab2d91bc9a
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-06 17:09:34 +0900

    S10P12B303-386-feat-mainuser componenet

---
+++ frontend/src/components/main/MainUser.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainFirst from '@/components/main/MainFirst.vue'
+import MainTopBanner from '@/components/main/MainTopBanner.vue'
+import MainBook from '@/components/main/MainBook.vue'
+</script>
+
+<template>
+  <div>
+    <MainFirst />
+    <MainBook />
+    <MainTopBanner />
+  </div>
+</template>
+
+<style></style>
