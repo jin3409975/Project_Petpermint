cfa40ff08c9ca864c0c41c9fdd3db1d041682053
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-06 17:08:33 +0900

    S10P12B303-386-feat-useless code deleted

--- frontend/src/components/main/Main.vue
+++ frontend/src/components/main/Main.vue
@@ -1,7 +1,6 @@
 <script setup>
 import MainFirst from '@/components/main/MainFirst.vue'
 import MainTopBanner from '@/components/main/MainTopBanner.vue'
-import MainLifeBanner from '@/components/main/MainLifeBanner.vue'
 import Typed from 'typed.js'
 </script>
 
@@ -10,39 +9,6 @@ import Typed from 'typed.js'
     <MainFirst />
     <MainTopBanner />
   </div>
-
-  <!-- <MainLifeBanner /> -->
 </template>
 
-<style>
-.main-container {
-  position: relative;
-  overflow: hidden;
-  height: 100vh;
-}
-
-.background-video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* 비디오가 컨테이너를 꽉 채우도록 */
-}
-
-.video-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: #fff;
-  text-align: center;
-  font-size: 50px;
-  font-weight: bold;
-}
-</style>
+<style></style>
