69c91b916c83b06530c5380a0b08e827cf504b0f
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-07 03:29:05 +0900

    S10P12B303-388-feat-emergency button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import NavbarMain from '@/components/main/NavbarMain.vue'
 import Footer from '@/components/main/Footer.vue'
 import { computed } from 'vue'
@@ -7,6 +8,17 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 const hideNavbarMain = computed(() => route.meta.hideNavbarMain)
 const hideFooter = computed(() => route.meta.hideFooter)
+const hideicon = computed(() => route.meta.hideicon)
+
+const defaultImage = '/assets/img/emergency.png'
+const hoverImage = '/assets/img/emergency_hover.png'
+const currentImage = ref(defaultImage)
+const handleMouseOver = () => {
+  currentImage.value = hoverImage
+}
+const handleMouseOut = () => {
+  currentImage.value = defaultImage
+}
 </script>
 
 <template>
@@ -15,8 +27,48 @@ const hideFooter = computed(() => route.meta.hideFooter)
       <NavbarMain v-if="!hideNavbarMain" />
       <RouterView />
       <Footer v-if="!hideFooter" />
+      <div class="emerg-container" @mouseover="handleMouseOver" @mouseout="handleMouseOut">
+        <img :src="currentImage" class="img-style" />
+        <span>응급예약</span>
+      </div>
     </v-app>
   </body>
 </template>
 
-<style></style>
+<style scoped>
+.emerg-container {
+  position: fixed;
+  bottom: 15px;
+  right: 20px;
+  z-index: 1000;
+  cursor: pointer;
+  width: 150px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: rgb(244, 46, 46) 2px solid;
+  border-radius: 15px;
+}
+
+.img-style {
+  width: auto;
+  height: 70%;
+  margin-right: 4px;
+}
+
+.emerg-container:hover {
+  background-color: #d2e0fb;
+  border-radius: 15px;
+  border: #d2e0fb 2px solid;
+}
+span {
+  white-space: nowrap;
+  font-weight: bold;
+  font-size: 25px;
+  color: rgb(244, 46, 46);
+}
+.emerg-container:hover span {
+  color: black;
+}
+</style>
