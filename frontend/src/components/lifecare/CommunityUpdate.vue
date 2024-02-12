b050c4d350e918af0425ec90b9eab87f353939be
CHAEEUN LEE <cheeun0903@gmail.com>
2024-02-13 04:06:35 +0900

    S10P12B303-294-style-button size changed

--- frontend/src/components/lifecare/CommunityUpdate.vue
+++ frontend/src/components/lifecare/CommunityUpdate.vue
@@ -53,61 +53,59 @@ export default {
 </script>
 
 <template>
-  <v-row justify="center" style="margin-right: 50px; margin-top: 1px">
-    <v-dialog v-model="dialog" persistent width="1024">
-      <template v-slot:activator="{ props }">
-        <v-btn
-          @click="logincheck"
-          v-bind="props"
-          style="margin-bottom: 5px"
-          density="compact"
-          icon="mdi-tooltip-edit"
-        ></v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5"><b>수정</b></span>
-        </v-card-title>
-        <form id="form-register" method="POST" action="">
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  readonly
-                  label="사용자 Email"
-                  v-model="userId"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-textarea
-                  clearable
-                  variant="outlined"
-                  label="내용*"
-                  required
-                  v-model="content"
-                ></v-textarea>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-file-input
-                  variant="outlined"
-                  multiple
-                  label="첨부 이미지"
-                  v-model="files"
-                ></v-file-input>
-              </v-col>
-            </v-row>
-            <small>*은 필수적으로 기입해야 합니다.</small>
-          </v-container>
-        </form>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue-darken-1" variant="text" @click="dialog = false"> Close </v-btn>
-          <v-btn color="blue-darken-1" variant="text" @click="update"> Save </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+  <v-dialog v-model="dialog" persistent width="1024">
+    <template v-slot:activator="{ props }">
+      <v-btn
+        @click="logincheck"
+        v-bind="props"
+        style="margin-bottom: 5px"
+        density="comfortable"
+        color="grey"
+        icon="mdi-square-edit-outline"
+      ></v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5"><b>수정</b></span>
+      </v-card-title>
+      <form id="form-register" method="POST" action="">
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field readonly label="사용자 Email" v-model="userId" required></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                clearable
+                variant="outlined"
+                label="내용*"
+                required
+                v-model="content"
+              ></v-textarea>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-file-input
+                variant="outlined"
+                multiple
+                label="첨부 이미지"
+                v-model="files"
+              ></v-file-input>
+            </v-col>
+          </v-row>
+          <small>*은 필수적으로 기입해야 합니다.</small>
+        </v-container>
+      </form>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="dialog = false"> Close </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="update"> Save </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(.v-btn .v-icon) {
+  --v-icon-size-multiplier: 1.2;
+}
+</style>
