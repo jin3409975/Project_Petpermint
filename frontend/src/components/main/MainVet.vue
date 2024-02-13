774acf2d8d4f7f9475d151fc4fbcfca9ed6cdfd9
이수형 <sky500ro@gmail.com>
2024-02-13 15:49:47 +0900

    S10P12B303-fix

--- frontend/src/components/main/MainVet.vue
+++ frontend/src/components/main/MainVet.vue
@@ -169,7 +169,7 @@ const handleEnteringConsultButton = () => {
     <div>
       <v-dialog v-model="dialog" width="400px">
         <!-- 온라인 초기 상담 -->
-        <v-carousel height="480" hide-delimiter-background show-arrows="hover" color="#668BA7">
+        <v-carousel height="480" hide-delimiter-background show-arrows="false" color="#668BA7">
           <v-carousel-item>
             <div class="card">
               <div class="card__img">
@@ -336,5 +336,244 @@ const handleEnteringConsultButton = () => {
 .button:active {
   background-color: #004b9b; /* Even darker color on click */
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* Smaller shadow on click */
+}
+
+.closed {
+  position: absolute;
+  top: 0;
+  height: 40px;
+  left: 105%;
+  transform: translateX(-50%);
+  z-index: 15;
+  border-top-right-radius: 20px;
+}
+.fixed-top-right {
+  position: fixed;
+  top: 100px;
+  right: 5px;
+  z-index: 1000;
+}
+.rows {
+  margin-left: 20px;
+  margin-right: 20px;
+  padding-bottom: 10px;
+  margin-top: -40px;
+}
+.margins {
+  padding-top: 0px;
+  padding-bottom: 0px;
+}
+.card__content {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.card__title {
+  margin-right: 20px;
+  font-weight: bold;
+  font-size: 17px;
+  color: var(--main-color);
+}
+
+.card__subtitle {
+  font-weight: bold;
+  font-size: 17px;
+  color: var(--submain-color);
+}
+
+.card {
+  --main-color: #000;
+  --submain-color: #78858f;
+  --bg-color: #fff;
+  font-family:
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+  position: relative;
+  width: 400px;
+  height: 480px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  background: var(--bg-color);
+}
+
+.card__img {
+  height: 192px;
+  width: 100%;
+}
+
+.card__img svg {
+  height: 100%;
+  border-radius: 20px 20px 0 0;
+}
+
+.card__avatar {
+  position: absolute;
+  width: 114px;
+  height: 114px;
+  background: var(--bg-color);
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: calc(18% - 57px);
+}
+
+.card__avatar svg {
+  width: 100px;
+  height: 100px;
+}
+
+.card__btn {
+  margin-top: 15px;
+  width: 76px;
+  height: 31px;
+  border: 2px solid var(--main-color);
+  border-radius: 4px;
+  font-weight: 700;
+  font-size: 11px;
+  color: var(--main-color);
+  background: var(--bg-color);
+  text-transform: uppercase;
+  transition: all 0.3s;
+}
+
+.card__btn-solid {
+  background: var(--main-color);
+  color: var(--bg-color);
+}
+
+.card__btn:hover {
+  background: var(--main-color);
+  color: var(--bg-color);
+}
+
+.card__btn-solid:hover {
+  background: var(--bg-color);
+  color: var(--main-color);
+}
+
+.my-card {
+  margin-bottom: 7px;
+  color: #474747;
+}
+
+.div {
+  z-index: 1000;
+  position: fixed;
+  top: 50%;
+  left: 0;
+  transform: translateY(-120%) translateX(20%);
+  width: 30px;
+  height: 50;
+  background-color: rgb(255, 0, 0);
+}
+.text-rot {
+  z-index: 1000;
+  position: fixed;
+  top: 50%;
+  left: 0;
+  transform: translateY(-120%) translateX(20%);
+  font-weight: bold;
+  color: #868484;
+}
+
+.draggable {
+  background-color: transparent;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.15);
+  width: 60px;
+  height: 80px;
+  overflow: hidden;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 300;
+  user-select: none;
+  box-shadow: none;
+  cursor: pointer;
+  z-index: 1000;
+  position: fixed;
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+}
+.bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+}
+.header,
+.content {
+  position: relative;
+  z-index: 1;
+  box-sizing: border-box;
+}
+.header {
+  width: 120px;
+  color: #373737;
+  height: 30px;
+  font-size: 13px;
+  font-weight: bold;
+  transform: rotate(90deg) translateY(170%) translateX(20%);
+}
+.arrow {
+  position: absolute;
+  top: 120%;
+  left: 24%;
+  transform: translate(-50%, -50%);
+  transform: rotate(-90deg);
+  cursor: pointer;
+}
+
+.arrow span {
+  display: block;
+  width: 1.1vw;
+  height: 1.1vw;
+  border-bottom: 4px solid #868484;
+  border-right: 4px solid #868484;
+  transform: rotate(45deg);
+  margin: -10px;
+  animation: animate 2s infinite;
+}
+
+.arrow span:nth-child(2) {
+  animation-delay: -0.2s;
+}
+
+.arrow span:nth-child(3) {
+  animation-delay: -0.4s;
+}
+
+@keyframes animate {
+  0% {
+    opacity: 0;
+    transform: rotate(45deg) translate(-20px, -20px);
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: rotate(45deg) translate(20px, 20px);
+  }
+}
+
+button {
+  width: 110px;
+  height: 50px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
 }
 </style>
