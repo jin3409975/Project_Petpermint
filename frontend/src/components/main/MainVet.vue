947b3389c0329371944c4ba1f8ebe360f324b995
govltjsdnd24 <gragasolaf@gmail.com>
2024-02-14 09:05:48 +0900

    S10P11B303-429-Broadcast-correction

--- frontend/src/components/main/MainVet.vue
+++ frontend/src/components/main/MainVet.vue
@@ -6,6 +6,7 @@ import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
+import Navbar from '@/components/main/NavbarMain.vue'
 
 const router = useRouter()
 const { VITE_APP_SERVER_URI } = import.meta.env
@@ -131,41 +132,53 @@ const handleEnteringConsultButton = () => {
 </script>
 
 <template>
-  <div style="">
-    <div></div>
+  <div style="text-align: center">
+    <Navbar />
+
     <div style="margin-top: 100px">
-      <VueDatePicker
-        v-model="date"
-        :start-time="startTime"
-        minutes-increment="20"
-        inline
-        time-picker-inline
-        auto-apply
-        :disabled-times="disabledTimes"
-        :disabled-dates="disabledDates"
-        :enable-time-picker="false"
-      >
-      </VueDatePicker>
+      <h1>수의사 예약 확인</h1>
     </div>
-    <div>
-      <v-card class="mx-auto hospital-list-card" max-width="450" style="border: 1px solid #36577e">
-        <v-list base-color="#36577e" three-line class="scrollable-list">
-          <v-list-item
-            v-for="(reserve, i) in reserveList"
-            :key="i"
-            :value="reserve"
-            @click="selectReservation(i)"
-          >
-            <v-list-item-content>
-              <v-list-item-title v-text="reserve.time"></v-list-item-title>
-              <v-list-item-subtitle
-                v-text="'보호자 : ' + userName[i] + ' / ' + '반려동물 : ' + animalName[i]"
-              ></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </div>
+    <v-col>
+      <div class="calendar-container" style="margin-right: 600px; margin-left: 550px">
+        <VueDatePicker
+          v-model="date"
+          :start-time="startTime"
+          minutes-increment="20"
+          inline
+          time-picker-inline
+          auto-apply
+          :disabled-times="disabledTimes"
+          :disabled-dates="disabledDates"
+          :enable-time-picker="false"
+          locale="ko-KR"
+        >
+        </VueDatePicker>
+      </div>
+
+      <div>
+        <v-card
+          class="mx-auto hospital-list-card"
+          max-width="450"
+          style="border: 1px solid #36577e; margin-top: 30px; margin-bottom: 30px"
+        >
+          <v-list base-color="#36577e" three-line class="scrollable-list">
+            <v-list-item
+              v-for="(reserve, i) in reserveList"
+              :key="i"
+              :value="reserve"
+              @click="selectReservation(i)"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="reserve.time"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="'보호자 : ' + userName[i] + ' / ' + '반려동물 : ' + animalName[i]"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </div>
+    </v-col>
     <div>
       <v-dialog v-model="dialog" width="400px">
         <!-- 온라인 초기 상담 -->
@@ -417,95 +430,6 @@ const handleEnteringConsultButton = () => {
   border-radius: 20px 20px 0 0;
 }
 
-.card__avatar {
-  position: absolute;
-  width: 114px;
-  height: 114px;
-  background: var(--bg-color);
-  border-radius: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: calc(18% - 57px);
-}
-
-.card__avatar svg {
-  width: 100px;
-  height: 100px;
-}
-
-.card__btn {
-  margin-top: 15px;
-  width: 76px;
-  height: 31px;
-  border: 2px solid var(--main-color);
-  border-radius: 4px;
-  font-weight: 700;
-  font-size: 11px;
-  color: var(--main-color);
-  background: var(--bg-color);
-  text-transform: uppercase;
-  transition: all 0.3s;
-}
-
-.card__btn-solid {
-  background: var(--main-color);
-  color: var(--bg-color);
-}
-
-.card__btn:hover {
-  background: var(--main-color);
-  color: var(--bg-color);
-}
-
-.card__btn-solid:hover {
-  background: var(--bg-color);
-  color: var(--main-color);
-}
-
-.my-card {
-  margin-bottom: 7px;
-  color: #474747;
-}
-
-.div {
-  z-index: 1000;
-  position: fixed;
-  top: 50%;
-  left: 0;
-  transform: translateY(-120%) translateX(20%);
-  width: 30px;
-  height: 50;
-  background-color: rgb(255, 0, 0);
-}
-.text-rot {
-  z-index: 1000;
-  position: fixed;
-  top: 50%;
-  left: 0;
-  transform: translateY(-120%) translateX(20%);
-  font-weight: bold;
-  color: #868484;
-}
-
-.draggable {
-  background-color: transparent;
-  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.15);
-  width: 60px;
-  height: 80px;
-  overflow: hidden;
-  text-align: center;
-  font-size: 14px;
-  font-weight: 300;
-  user-select: none;
-  box-shadow: none;
-  cursor: pointer;
-  z-index: 1000;
-  position: fixed;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-}
 .bg {
   position: absolute;
   top: 0;
